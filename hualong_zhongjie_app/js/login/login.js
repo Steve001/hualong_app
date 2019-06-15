04c50e0d69c2d5969002a2706314581b5797b977
DESKTOP-E478071\lzj <976623696@qq.com>
2019-06-15 14:39:27 +0800

    客户端增加检测更新

--- hualong_zhongjie_app/js/login/login.js
+++ hualong_zhongjie_app/js/login/login.js
@@ -4,6 +4,12 @@ var backurl = "../" + backid;
 mui.plusReady(function() {
 
 	storage.init();
+	storageUser = kidstorageuser.getInstance();
+
+	//获取版本信息
+	//得到当前版本
+	var version = plus.runtime.version;
+	document.getElementById("version").innerText = version;
 
 	backid = appPage.getParam("backid") || "main.html";
 	backurl = "../" + backid;
@@ -12,12 +18,56 @@ mui.plusReady(function() {
 	var inpt_mobile = document.getElementById("inpt_mobile");
 	var inpt_pwd = document.getElementById("inpt_pwd");
 
-	storageUser = kidstorageuser.getInstance();
-	
-	if( storageUser.IsLogin){
-		openNew("../main.html");
-		return;
+	var data = {
+		'version': version,
+		'platform': plus.os.name
 	}
+
+	//检测更新
+	request("/checkVersion", data, function(json) {
+		if(json.status == "success") {
+			var data = json.appdata[0];
+			if(data.version != '0') {
+				var w = plus.nativeUI.showWaiting("正在更新资源...");
+				//说明有更新
+				var dtask = plus.downloader.createDownload(data.apk, {}, function(d, status) {
+					if(status == 200) {
+						// 下载成功
+						var path = d.filename;
+						plus.runtime.install(path); // 安装下载的apk文件  
+					} else { //下载失败  
+						alert("Download failed: " + status);
+					}
+				});
+				dtask.addEventListener("statechanged", function(task, status) {
+					switch(task.state) {
+						case 1: // 开始  
+							w.setTitle("　　 开始下载...　　 ");
+							break;
+						case 2: // 已连接到服务器  
+							w.setTitle("　　 开始下载...　　 ");
+							break;
+						case 3:
+							var a = task.downloadedSize / task.totalSize * 100;
+							w.setTitle("　　 已下载" + parseInt(a) + "%　　 ");
+							break;
+						case 4: // 下载完成  
+							w.close();
+							break;
+					}
+				});
+				dtask.start();
+			} else {
+				//无更新
+				if(storageUser.IsLogin) {
+					openNew("../main.html");
+					return;
+				}
+			}
+		}
+	}, false, function() {
+		plus.nativeUI.closeWaiting();
+	});
 
 	btn_login.addEventListener("tap", function() {
 		var data = {
@@ -54,5 +104,4 @@ mui.plusReady(function() {
 	document.getElementById("btn_reg").addEventListener("tap", function() {
 		openNew("reg.html");
 	});
-	//手机登录
 })
